4b3b9df9f75e8efea22daaa481bc2bde834a4c58
Neka <nekoanchor@yahoo.fr>
2023-03-04 23:08:41 +0100

    feat: setup dotenv & add favicon

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import nodemailer from "nodemailer";
 import fetch from "node-fetch";
 const app = express();
 const port = 3333;
+
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, SMTP_SECURE } = process.env;
+
+const transporter = nodemailer.createTransport({
+  host: SMTP_HOST,
+  port: SMTP_PORT,
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASS,
+  },
+});
 
 app.get("/", (req, res) => {
   return res
@@ -13,8 +26,8 @@ app.get("/", (req, res) => {
 app.get("/one-mail/:email", async (req, res) => {
   const email = req.params.email;
   console.log("verifyDomain", verifyDomain(email));
-  console.log("checkif", (await verifyEmailComposition(email)) , (await verifyDomain(email)))
-  if ((await verifyEmailComposition(email)) && (await verifyDomain(email))) {
+  console.log("checkif", await verifyEmail(email))
+  if ((await verifyEmailComposition(email)) && (await verifyDomain(email)) && (await verifyEmail(email))) {
     return res
       .status(200)
       .json({ success: true, message: `${email} is a valid email address.` });
@@ -68,27 +81,16 @@ const verifyDomain = async (email) => {
 
 
 const verifyEmail = async (email) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.example.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "your_username",
-      pass: "your_password",
-    },
-  });
-
   try {
-    const info = await transporter.verify();
-    console.log("SMTP server is ready to send mails.");
+    await transporter.verify();
     const result = await transporter.verifyRecipient(email);
-    console.log("Mail address is valid and active.");
     return true;
   } catch (error) {
-    console.log("Erreur : " + error);
+    console.error('Error: ' + error.message);
     return false;
   }
 };
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
