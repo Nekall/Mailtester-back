a0ee37b7e5b7a625ff513bc1c69c546a0ba5719d
Neka <nekoanchor@yahoo.fr>
2023-03-08 23:39:13 +0000

    update: rework all app

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import nodemailer from "nodemailer";
-import fetch from "node-fetch";
+import { validate } from "deep-email-validator";
 const app = express();
 const port = 3333;
-
-const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, SMTP_SECURE } = process.env;
-
-const transporter = nodemailer.createTransport({
-  host: SMTP_HOST,
-  port: SMTP_PORT,
-  auth: {
-    user: SMTP_USER,
-    pass: SMTP_PASS,
-  },
-});
 
 app.get("/", (req, res) => {
   return res
@@ -25,71 +13,41 @@ app.get("/", (req, res) => {
 
 app.get("/one-mail/:email", async (req, res) => {
   const email = req.params.email;
-  //console.log("checkif", await verifyEmail(email))
-  if ((await verifyEmailComposition(email)) && (await verifyDomain(email))) {
+  let checkAll = await validate({
+    email: email,
+    sender: email,
+    validateRegex: true,
+    validateMx: true,
+    validateTypo: false,
+    validateDisposable: true,
+    validateSMTP: true,
+  });
+  if (checkAll.valid) {
     return res
       .status(200)
-      .json({ success: true, message: `${email} is a valid email address.` });
+      .json({
+        success: true,
+        message: `${email} is a valid email address.`,
+        details: checkAll, });
   }
   return res
     .status(406)
     .json({
       success: false,
       message: `${email} is NOT a valid email address.`,
+      details: checkAll,
     });
 });
 
 app.post("/multiple-mails", (req, res) => {
-    const emails = req.body.emails;
-    const validEmails = [];
-    const invalidEmails = [];
-    emails.forEach((email) => {
-        if (verifyEmailComposition(email)) {
-            if (verifyDomain(email)) {
-                validEmails.push(email);
-            } else {
-                invalidEmails.push(email);
-            }
-        } else {
-            invalidEmails.push(email);
-        }
-    });
-    return res.send({
-        validEmails: validEmails,
-        invalidEmails: invalidEmails,
+    //const emails = req.body.emails;
+    return res
+    .status(418)
+    .json({
+      success: true,
+      message: "Not implemented yet",
     });
 });
-
-const verifyEmailComposition = async (email) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
-
-const verifyDomain = async (email) => {
-  const url = new URL(`https://${email.split("@")[1]}`);
-  let valid = false;
-  try {
-    let response = await fetch(url);
-    valid = response.ok;
-  } catch {
-    console.error("Domain is not valid.");
-    valid = false;
-  }
-  return valid;
-};
-
-
-const verifyEmail = async (email) => {
-  try {
-    await transporter.verify();
-    const result = await transporter.verifyRecipient(email);
-    return true;
-  } catch (error) {
-    console.error('Error: ' + error.message);
-    return false;
-  }
-};
-
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
