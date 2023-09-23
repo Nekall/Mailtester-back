f56fa18e427952b164bcdaf2b566185d36909507
Neka <nekoanchor@yahoo.fr>
2023-09-23 11:33:39 +0200

    feat: add bulk emails check

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import express from "express";
 import net from "net";
 import dns from "dns";
 const app = express();
-const port = 3333;
+import cors from "cors";
+import bodyParser from "body-parser";
+const PORT = process.env.PORT || 3333;
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get("/", (_, res) => {
   return res
@@ -16,18 +24,18 @@ const validateEmail = (email) => {
     return Promise.resolve(false);
   }
 
-  const domain = email.split('@')[1];
+  const domain = email.split("@")[1];
   const ports = [25, 587, 465];
-  
+
   return new Promise((resolve) => {
     const tryNextPort = (index) => {
       if (index >= ports.length) {
         resolve(false);
         return;
       }
-      
+
       const port = ports[index];
-      
+
       dns.resolveMx(domain, (err, mxRecords) => {
         if (err || !mxRecords || mxRecords.length === 0) {
           tryNextPort(index + 1);
@@ -41,19 +49,19 @@ const validateEmail = (email) => {
               tryNextPort(index + 1);
             }
           }, timeout);
-          smtp.on('connect', () => {
+          smtp.on("connect", () => {
             connected = true;
             clearTimeout(timeoutId);
             smtp.destroy();
             resolve(true);
           });
-          smtp.on('error', () => {
+          smtp.on("error", () => {
             tryNextPort(index + 1);
           });
         }
       });
     };
-    
+
     tryNextPort(0);
   });
 };
@@ -62,46 +70,79 @@ app.get("/single-mail/:email", async (req, res) => {
   const email = req.params.email;
 
   validateEmail(email)
-  .then((response) => {
-    if(response){
-      return res
-        .status(200)
-        .json({
+    .then((response) => {
+      if (response) {
+        return res.status(200).json({
           success: true,
-          message: `${email} is a valid email address.`
+          message: `${email} is a valid email address.`,
         });
-    } else {
-      return res
-        .status(406)
-        .json({
-          success: false, 
-          message: `${email} is NOT a valid email address.`
+      } else {
+        return res.status(406).json({
+          success: false,
+          message: `${email} is NOT a valid email address.`,
         });
-    }
-  })
-  .catch((err) => {
-    return res
-      .status(406)
-      .json({
+      }
+    })
+    .catch((err) => {
+      return res.status(406).json({
         success: false,
         message: `${email} is NOT a valid email address.`,
-        error: err
+        error: err,
       });
-  });
-});
-
-
-app.post("/bulk-mails", (req, res) => {
-    const emails = req.body.emails;
-    
-    return res
-    .status(418)
-    .json({
-      success: true,
-      message: "Not implemented yet",
     });
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.post("/bulk-mails", (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({
+      success: false,
+      message: "Body must be provided",
+    });
+  }
+
+  if (!req.body.emails) {
+    return res.status(400).json({
+      success: false,
+      message: "Emails must be provided",
+    });
+  }
+  const emails = req.body.emails;
+
+  if (!Array.isArray(emails)) {
+    return res.status(400).json({
+      success: false,
+      message: "Emails must be an array",
+    });
+  }
+
+  const validEmails = [];
+  const invalidEmails = [];
+
+  for (let email of emails) {
+    validateEmail(email)
+      .then((response) => {
+
+        if (response) {
+          validEmails.push(email);
+        } else {
+          invalidEmails.push(email);
+        }
+        
+        if(emails.length === validEmails.length + invalidEmails.length) {
+          return res.status(200).json({
+            success: true,
+            message: "Emails validated",
+            validEmails: validEmails,
+            invalidEmails: invalidEmails,
+          });
+        }
+      })
+      .catch(_ => {
+        invalidEmails.push(email);
+      });
+  } 
+});
+
+app.listen(PORT, () => {
+  console.info(`App listening at http://localhost:${PORT}`);
 });
